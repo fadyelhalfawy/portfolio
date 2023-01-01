92f546640bc304a691a011567bf57e00f3c22343
Fady ElHalfawy <fadyhalfawy@gmail.com>
2023-01-01 15:23:48 +0200

    Update Navbar component by adding icons and list the links of navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,10 @@
-import React from "react";
-import {faBars, faTimes} from "react-icons/fa"
+import React, {useState} from "react";
+import {FaBars, FaTimes} from "react-icons/fa"
 
 
 export const NavBar = () => {
+
+    const [nav, setNav] = useState(false);
 
     const links = [
         {
@@ -28,8 +30,8 @@ export const NavBar = () => {
     ]
 
     return (
-        <div>
-            <div className="flex justify-between items-center w-full h-20 px-4 bg-black text-white fixed">
+        <div className="flex justify-between items-center w-full h-20 px-4 bg-black text-white fixed">
+            <div>
                 <h1 className="text-5xl font-signature ml-2">Fady ElHalfawy</h1>
             </div>
 
@@ -41,6 +43,10 @@ export const NavBar = () => {
                        </li>
                    ))}
             </ul>
+
+            <div onClick={() => setNav(!nav)} className={"cursor-pointer pr-4 z-10 text-gray-500"}>
+                {nav ? <FaTimes size={30}/> : <FaBars size={30} /> }
+            </div>
         </div>
     )
 }
