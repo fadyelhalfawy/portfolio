8dc93dbdfbe8b9d19e7114bbe589db883944a357
Fady ElHalfawy <fadyhalfawy@gmail.com>
2023-01-14 03:27:47 +0200

    Update these files id link to scroll down to any one of them

--- src/components/Home.js
+++ src/components/Home.js
@@ -21,7 +21,7 @@ export const Home = () => {
                         concept and participate in latest technologies.
                     </p>
                     <div>
-                        <Link to={"Portfolio"} smooth duration={500} className={"group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"}>
+                        <Link to={"portfolio"} smooth duration={500} className={"group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"}>
                             Portfolio
                             <span className={"group-hover:rotate-90 duration-300"}>
                                 <BsArrowRightCircleFill className={"ml-1"} size={20}/>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 import {FaBars, FaTimes} from "react-icons/fa";
-import {Link} from "react-scroll";
+import {NavLinks} from "./NavLinks";
 
 
 export const NavBar = () => {
@@ -50,11 +50,12 @@ export const NavBar = () => {
             {nav &&
                 <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
                     {links.map(({ id, link }) => (
-                        <li key={id} className={"px-4 cursor-pointer capitalize py-6 text-4xl"}>
-                            <Link onClick={() => setNav(!nav)} to={link} smooth duration={500}>
-                                {link}
-                            </Link>
-                        </li>
+                        <NavLinks
+                            key={id}
+                            link={link}
+                            className={"px-4 cursor-pointer capitalize py-6 text-4xl"}
+                            onClick={() => setNav(!nav)}
+                        />
                     ))}
                 </ul>
             }
